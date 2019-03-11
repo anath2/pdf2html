32056d47450f62bc991d98137ea2e10a296b8d1f
Ajit Nath <nath.ajit01@gmail.com>
2019-03-11 20:27:11 +0800

    Add blueprint to application factory

--- pdf2html/__init__.py
+++ pdf2html/__init__.py
@@ -5,12 +5,15 @@ Application entry point
 import os
 from flask import Flask
 
+from . import index
+
 
 def create_app(test_config=None):
     '''
     Application factory
     '''
     app = Flask(__name__, instance_relative_config=True)
+    app.register_blueprint(index.bp)
 
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -23,9 +26,5 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py', silent=True)
 
     os.makedirs(app.instance_path, exist_ok=True)
-
-    @app.route('/')
-    def test():
-        return 'Hello world!'
 
     return app
