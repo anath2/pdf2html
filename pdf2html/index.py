6f2e64d2dbbc58bdb74a218ec8f3278ef8e93299
Ajit Nath <nath.ajit01@gmail.com>
2019-03-12 21:23:40 +0800

    Adds 'about' view

--- pdf2html/index.py
+++ pdf2html/index.py
@@ -1,14 +1,29 @@
 '''
-Application entry point
+Entrypoint view
 '''
 
 import functools
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, redirect
 
+from . import core
 
 bp = Blueprint('pdf2html', __name__)
 
 
 @bp.route('/', methods=('GET', 'POST'))
 def index():
+    # Algorithm
+    if request.method == 'POST':
+        path = request.form['file']
+
+        # Generate expireable random string url
+        file_url = 'SOMETHING'
+        redirect(file_url)
+
     return render_template('index.html')
+
+
+@bp.route('/about', methods=('GET',))
+def about():
+    # Render about page
+    pass
