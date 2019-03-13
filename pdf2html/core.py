48772e0852d89f80be74a430dfd8ebf07602b222
Ajit Nath <nath.ajit01@gmail.com>
2019-03-13 23:50:48 +0800

    use alphanum string

--- pdf2html/core.py
+++ pdf2html/core.py
@@ -5,7 +5,7 @@ Core module
 import random
 
 
-def generate_random_url(base_url, len=12):
+def generate_random_url(base_url, strlen=12):
 
     '''
     ARGS:
@@ -13,8 +13,6 @@ def generate_random_url(base_url, len=12):
         length: length of the random string
     Generates a url with random letters
     '''
-    start = 65  # ASCII 'A'
-    end = 127  # Last letter
-
-    random_str = ''.join([chr(random.randrange(start, end)) for l in range(len)])
+    alphanum = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    random_str = ''.join(alparange[random.randrange(len(alphanum))] for l in range(strlen)])
     return base_url + '/' + random_str
